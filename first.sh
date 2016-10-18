6afe84dfd2c30aa07d468bda50f2f24d81105b61
lcxaiw <965955491@qq.com>
2016-10-18 17:19:09 +0800

    origin

---
+++ first.sh
@@ -0,0 +1,19 @@
+sudo apt -y install gdb
+sudo apt-get -y install vim
+sudo apt-get -y install screen
+sudo apt-get -y install git
+sudo apt-get -y install ctags
+sudo apt-get -y install gcc
+sudo apt-get -y install g++
+sudo apt-get -y install make
+sudo apt -y install cmake
+sudo apt-get -y install unzip
+sudo apt -y install linux-tools-common
+sudo apt -y install linux-tools-4.4.0-21-generic
+sudo apt -y install linux-cloud-tools-4.4.0-21-generic
+sudo apt -y install linux-tools-generic
+sudo apt -y install linux-cloud-tools-generic
+sudo apt -y install linux-tools-4.4.0-36-generic
+sudo apt -y install linux-cloud-tools-4.4.0-36-generic
+
+
---
+++ stardict.sh
@@ -0,0 +1,7 @@
+#to install a dictionary to help read papers
+#to open the softward ,just type stardict on the shell. And twice stick on
+#the word you want to know.It will show its meaning to you
+sudo apt-get install stardict
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-oxford-gb-2.4.2.tar.bz2
+tar -xvjf stardict-oxford-gb-2.4.2.tar.bz2
+sudo mv stardict-oxford-gb-2.4.2 /usr/share/stardict/dic/
